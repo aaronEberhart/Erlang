806165aa168751e920b9885fd7addfcb78238a8f
aaron <aaron@aaron-Lenovo-Yoga-2-11>
2018-09-05 10:40:20 -0400

    message

--- Aarlang/src/program.erl
+++ Aarlang/src/program.erl
@@ -7,9 +7,28 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([]).
+-export([list_plus_one/1, funky_add/2]).
 
+%% ====================================================================
+%% External functions
+%% ====================================================================
+list_plus_one([ ]) -> 
+	[ ];
+list_plus_one([ H | T ]) when (H + 1) rem 5 == 0 -> 
+	list_plus_one( T );
+list_plus_one([ H | T ]) -> 
+	[ H + 1 | list_plus_one( T ) ];
+list_plus_one( _ ) -> 
+	"Inappropriate Input".
 
+	
+
+funky_add(X,Y) ->
+	if is_function(X) and is_function(Y) -> X() + Y();
+	   is_function(X) -> X() + Y;
+	   is_function(Y) -> X + Y();
+	   true -> X + Y
+	end.
 
 %% ====================================================================
 %% Internal functions
